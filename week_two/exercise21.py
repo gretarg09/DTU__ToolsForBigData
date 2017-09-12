a37e497c8473d0c73da20684e5bf51da11348e31
Gretar Atli Gretarsson <gretaratlig09@gmail.com>
2017-09-12 10:50:56 +0200

    exercise 2.1

---
+++ week_two/exercise21.py
@@ -0,0 +1,46 @@
+# Import the modules
+import sys
+import random
+
+
+# takes in integer i and returns binary representation, if n length, for the integer.
+def intToBinary(i,n):
+    l = [0] * n
+    if i == 0: return l
+    index = 0
+    while i:
+        if i & 1 == 1:
+            l[index] = 1
+        else:
+            l[index] = 0
+        i /= 2
+        index += 1
+    # Then reverse the list before returning
+    return l[::-1]
+
+# Creates all combination of list of n size that only contains 0 or 1
+def createLists(n):
+    for i in range (2**n):
+        print intToBinary(i,n)
+
+# Check if string is integer
+def representsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+ans = True
+
+while ans:
+    question = raw_input("Please enter a integer: (press enter to quit) ")
+    
+    if(question == ""):
+        break
+
+    if representsInt(question):
+        createLists(int(question))
+    else:
+        print "\nThis is not an integer \n"
+
