0a261de845528814fc086846deb2153aa19b01e3
Gretar Atli Gretarsson <gretaratlig09@gmail.com>
2017-09-18 17:26:07 +0200

    changed bitwise and operator into modulus

--- week_two/exercise21.py
+++ week_two/exercise21.py
@@ -6,10 +6,13 @@ import random
 # takes in integer i and returns binary representation, if n length, for the integer.
 def intToBinary(i,n):
     l = [0] * n # creates a list of zeros of n length
-    if i == 0: return l
+    if i == 0: return l #If n = 0 then we just output the zero list
     index = 0
+    # For each iteration we check if the integer is even or odd.
+    #   if the integer is even then the program adds 1 to the list l
+    #   if the integer is odd then the program adds 0 to the list l
     while i:
-        if i & 1 == 1:
+        if i % 2 == 1:
             l[index] = 1
         else:
             l[index] = 0
@@ -18,7 +21,7 @@ def intToBinary(i,n):
     # Then reverse the list before returning
     return l[::-1]
 
-# Creates all combination of list of n size that only contains 0 or 1
+# Creates all combination of list of n size that only contains 0 or 1 (binary list)
 def createLists(n):
     for i in range (2**n):
         print intToBinary(i,n)
