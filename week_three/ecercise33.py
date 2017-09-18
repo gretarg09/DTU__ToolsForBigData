544164fb51bea8e603bd1d96e05205b9ad4a1980
IngvarBjarki <ingvarbjarki@gmail.com>
2017-09-18 19:08:15 +0200

    dæmi 2 og 3

---
+++ week_three/ecercise33.py
@@ -0,0 +1,23 @@
+names = ['John', 'Tom', 'Snappy', 'John', 'Kitty', 'Jessie',
+         'John', 'Kitty', 'Chester', 'John', 'Doe', 'Tom', 'Stefano',
+         'Jenny', 'Anna', 'charles']
+
+the_dictonary = {}
+for name in names:
+    if name not in the_dictonary:
+        the_dictonary[name] = 1
+    else:
+        the_dictonary[name] += 1
+
+#lets also sort the dictonary according to value.. reverse prints the highest number first, the anyomus function
+#selects the second element which is the values in the the_dictonary.items()..
+for key, value in sorted(the_dictonary.items(), key=lambda x: x[1], reverse = True):
+    print "%s appers: %d"%(key, value)
+    
+for key, value in the_dictonary.items():
+    if value == 1:
+        del the_dictonary[key] #delete from the dictonary according to the key value
+print
+print "Solution:"
+for key, value in sorted(the_dictonary.items(), key=lambda x: x[1], reverse = True):
+    print "%s appers: %d"%(key, value)
