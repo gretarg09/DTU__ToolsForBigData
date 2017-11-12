4d7cae31a4e65edb77fa76b4d773cd650be24118
Gretar Atli Gretarsson <gretaratlig09@gmail.com>
2017-11-12 20:57:59 +0100

    bla

--- challenge_second/GAG/sql/ex2.py
+++ challenge_second/GAG/sql/ex2.py
@@ -16,7 +16,7 @@ cur = con.cursor()
 
 def getUniqNameListCount(subreddit_id):
 	#print("Running for {}".format(subreddit_id))
-	print (subreddit_id)
+	#print (subreddit_id)
 	cur.execute("SELECT author_id FROM comments WHERE subreddit_id = ?",[subreddit_id])
 	return ([i[0] for i in cur.fetchall()], subreddit_id)
 
@@ -25,72 +25,85 @@ def getUniqNameListCount(subreddit_id):
 if __name__ == '__main__':
 
 	print("########### Starting ################")
+	while True:
+		t1 = time.time()
 
-	t1 = time.time()
+		cur.execute("""SELECT subreddit_id, count(ROWID) 
+					   FROM comments 
+					   GROUP BY subreddit_id""")
 
-	cur.execute("""SELECT subreddit_id, count(ROWID) 
-				   FROM comments 
-				   GROUP BY subreddit_id""")
+		top_n = cur.fetchall()
+		top_n.sort(key=lambda x: x[1],reverse=True)
 
-	top_n = cur.fetchall()
-	top_n.sort(key=lambda x: x[1],reverse=True)
+		t2 = time.time()
 
-	t2 = time.time()
+		n = 120 # number of top subreddits Ids
 
-	n = 10 # number of top subreddits Ids
+		for i in top_n[0:n]:
+			print (i)
 
-	for i in top_n[0:n]:
-		print (i)
+		print("######### First step Finished #########")
+		print("Execution time {}".format(t2-t1))	
 
-	print("######### First step Finished #########")
-	print("Execution time {}".format(t2-t1))	
+		# Next step is to take the subreddit_ids that have the most comments 
+		# and find all of the authors that they do have in common 
 
-	# Next step is to take the subreddit_ids that have the most comments 
-	# and find all of the authors that they do have in common 
+		# First I get all the unique authors of these subreddit Ids
 
-	# First I get all the unique authors of these subreddit Ids
+		t3 = time.time()
 
-	t3 = time.time()
+		p = Pool(n)
+		results = p.map(getUniqNameListCount, [i[0] for i in top_n[0:n]])
+		#results = p.map(getUniqNameListCount, ['t5_2qh61', 't5_2t9x3'])
+		p.close()
+		#p.join()
 
-	p = Pool(n)
-	results = p.map(getUniqNameListCount, [i[0] for i in top_n[0:n]])
-	#results = p.map(getUniqNameListCount, ['t5_2qh61', 't5_2t9x3'])
-	p.close()
-	#p.join()
+		t4 = time.time()
 
-	t4 = time.time()
+		print("######### Second step Finished #########")
+		print("Execution time {}".format(t4-t3))
 
-	print("######### Second step Finished #########")
-	print("Execution time {}".format(t4-t3))
+		# Add the result into a default dict 
+		t5 = time.time()
+		top_n_distinctAuthors = defaultdict(set)
 
-	# Add the result into a default dict 
-	t5 = time.time()
-	top_n_distinctAuthors = defaultdict(set)
+		for result in results:
+			top_n_distinctAuthors[result[1]] = set(result[0])
 
-	for result in results:
-		top_n_distinctAuthors[result[1]] = set(result[0])
+		t6 = time.time()
 
-	t6 = time.time()
+		print("Adding dictionary execution time {}".format(t6-t5))
 
-	print("Adding dictionary execution time {}".format(t6-t5))
+		the_result = defaultdict(int)
 
-	the_result = defaultdict(int)
+		# Now we use iteritem combination to get find the the number of common authors between pairs
+		for i in combinations(top_n_distinctAuthors.keys(), 2):
+			the_result[i] = len(top_n_distinctAuthors[i[0]].intersection(top_n_distinctAuthors[i[1]] ))
 
-	# Now we use iteritem combination to get find the the number of common authors between pairs
-	for i in combinations(top_n_distinctAuthors.keys(), 2):
-		the_result[i] = len(top_n_distinctAuthors[i[0]].intersection(top_n_distinctAuthors[i[1]] ))
+		t7 = time.time()
 
-	t7 = time.time()
+		print("Finding the intersection execution time {}".format(t7-t6))
 
-	print("Finding the intersection execution time {}".format(t7-t6))
+		the_result_sorted = sorted(the_result.items(), key=operator.itemgetter(1), reverse = True)
 
-	the_result_sorted = sorted(the_result.items(), key=operator.itemgetter(1), reverse = True)
+		print("Check if the 10 highest stake is smaller then top n+1")
+
+		print("The result sorted {}".format(the_result_sorted[9][1]))
+		print("Top 121 {}".format(top_n[n+1][1]))
+
+		if top_n[n+1][1] <  the_result_sorted[9][1]:
+			break
+		else:
+			n = n + 20
+
 	
 	for key,value in the_result_sorted[0:10]:
 		name1 = cur.execute("SELECT name FROM subreddits WHERE id = ?",[key[0]]).fetchall()
 		name2 = cur.execute("SELECT name FROM subreddits WHERE id = ?",[key[1]]).fetchall()
-		print("{},{} - {}".format(name1,name2,value))
-	print("Overall execution time {}".format(t7-t1))
+		print("{},{} - {}".format(name1[0],name2[0],value))
+
+	t8 = time.time()
+	print("Overall execution time {}".format(t8-t1))
 
 
 
