b802492a70d72066652db1c7412507c9f84fcb26
Gretar Atli Gretarsson <gretaratlig09@gmail.com>
2017-11-28 13:07:41 +0100

    monday late work on tools for big data

--- challenge_third/GAG/challenge_3_GAG.py
+++ challenge_third/GAG/challenge_3_GAG.py
@@ -3,17 +3,25 @@ import cv2 # for this I needed to install opencv -> pip install opencv-python
 import numpy as np
 import json
 import time
+import math
 
 
-#filenames = [
-#        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4", 
-#        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/76KUS3QCGVCY.mp4", 
-#        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/96EC4QS20Z28.mp4", 
-#        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/CL8W7L333U90.mp4"]
+filenames = [
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4", 
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/76KUS3QCGVCY.mp4", 
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/96EC4QS20Z28.mp4", 
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/CL8W7L333U90.mp4",
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/FDAZ5NL5NFL2.mp4",
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/HBX8QLI9HH25.mp4",
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/JY2ZAINWD2RX.mp4",
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/LP47ZGJ256YU.mp4",
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/NTETO8P77N96.mp4",
+        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/SLK2PRXGW3DZ.mp4"]
 
-#filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4"]
+#filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4",
+#             "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/96EC4QS20Z28.mp4"] 
 
-filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/LVK4R8FJA3N9.mp4"]
+#filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/LVK4R8FJA3N9.mp4"]
 
 #filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4",
 #             "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/76KUS3QCGVCY.mp4"]
@@ -40,8 +48,8 @@ for file in filenames:
     
     sum_images = np.zeros((pxls,pxls + 1))
 
-    #print("--------------------------------------")
-    #print file
+    print("--------------------------------------")
+    print file
     
     while(True):
         # Capture frame-by-frame
@@ -51,38 +59,54 @@ for file in filenames:
             # Start analysing the frames
             
             #### CROP IMAGE ####
+            # Here we crop the black frame from the images
+            # There are two different cases, either a portreit image of a landscape image
             
-            height = np.size(frame, 0)
-            width = np.size(frame, 1)
+            crop = True
             
-            if height > width: # if portrait image
-                x = 256
-                y = 454                
-            else: # else it is a landscape 
-                x = 454
-                y = 256
-        
+            if crop:
+                height = np.size(frame, 0)
+                width = np.size(frame, 1)
+                
+                #print("height : {}".format(height))
+                #print("width : {}".format(width))
+                
+                
+                if height > width: # if portrait image
+                    x = 256
+                    y = 454                
+                else: # else it is a landscape 
+                    x = 454
+                    y = 256
             
-            x_start = width/2 - x/2
-            x_end = width/2 + x/2
-            y_start = height/2 - y/2
-            y_end = height/2 + y/2
-
+                x_start = int(width/2 - x/2)
+                x_end = int(width/2 + x/2)
+                y_start = int(height/2 - y/2)
+                y_end = int(height/2 + y/2)
+                
+                #print("x start : {}".format(x_start))
+                #print("x end : {}".format(x_end))
+                #print("y start : {}".format(y_start))
+                #print("y end : {}".format(y_end))
+    
+                
+                frame = frame[y_start:y_end, x_start:x_end]
             
-            crop_frame = frame[y_start:y_end, x_start:x_end]
             
-            #------------------#
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             
-            print (height,width)
+            #### Histogram normalization ####
             
-            cv2.imwrite('croppedframeur.png',crop_frame)
+            #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+            #gray = clahe.apply(gray)
+            #gray = cv2.equalizeHist(gray)
             
-            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #gray = cv2.resize(gray,(pxls + 1,pxls))
-            #sum_images = gray + sum_images
-
+            #cv2.imwrite('second.png',gray)
             
-            break
+            gray = cv2.resize(gray,(pxls + 1,pxls))
+            
+            sum_images = gray + sum_images
+            
             
         else:
             break
@@ -91,6 +115,7 @@ for file in filenames:
     cap.release()
     cv2.destroyAllWindows()
     
+    #print (sum_images)
     
     img_compared = np.empty((pxls,pxls))
     for i,row in enumerate(sum_images):
@@ -100,8 +125,8 @@ for file in filenames:
             else:
                 img_compared[i][j] = row[j] > row[j+1]
     
-    print "The full matrix"
-    print img_compared
+   # print "The full matrix"
+   #print img_compared
     
     hash_string = ""
     for difference in img_compared:
@@ -118,7 +143,7 @@ for file in filenames:
         hash_string = hash_string +''.join(hex_string) + ' '
     
     print hash_string
-    print int(hash_string.replace(" ", ""),16)
+    #print int(hash_string.replace(" ", ""),16)
     
     
     
