8b14e2fc45915d35eec84081716e77fbc21eb743
Gretar Atli Gretarsson <gretaratlig09@gmail.com>
2017-12-07 21:19:25 +0100

    The code made more understandable

--- challenge_third/GAG/challenge_3_Ingvar.py
+++ challenge_third/GAG/challenge_3_Ingvar.py
@@ -15,7 +15,7 @@ import os
 import imageio
 
 
-
+import adjusted_rand_index as rand_index_validation
 import sklearn.cluster as cluster
 from sklearn import mixture
 from sklearn.cluster import KMeans
@@ -25,153 +25,49 @@ from sklearn.preprocessing import StandardScaler
 from itertools import cycle, islice
 
 
-#from sklearn import mixture
-#from sklearn.cluster import KMeans
-#from sklearn import cluster, datasets, mixture
-#from sklearn.neighbors import kneighbors_graph
-#from sklearn.preprocessing import StandardScaler
-
-
-
-
-
-filenames = [
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4", 
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/76KUS3QCGVCY.mp4", 
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/96EC4QS20Z28.mp4", 
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/CL8W7L333U90.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/FDAZ5NL5NFL2.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/HBX8QLI9HH25.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/JY2ZAINWD2RX.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/LP47ZGJ256YU.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/NTETO8P77N96.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/SLK2PRXGW3DZ.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/LVK4R8FJA3N9.mp4"]
-
-filenames0 = [
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/6CY15JHMFHQ4.mp4", 
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/6NWTR5CP41WG.mp4", 
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/9H0UQ6RGPK51.mp4", 
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/9Y6TIK3P5MDO.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/LVK4R8FJA3N9.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/MF34IWZEV0H1.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/PZGZKNTRVEUH.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/QB9RBNGHAR91.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/SKGL1C7462UE.mp4",
-        "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/SM4TDHHC0FLL.mp4"
-        #"/Users/GretarAtli/Dropbox/ToolsForBigData/ur3/3FVFA1DVA3NZ.mp4"
-        ]
-
-
-filenames1 = ["/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4",
-             "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/96EC4QS20Z28.mp4"] 
-
-filenames2 = ["/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/LVK4R8FJA3N9.mp4"]
-
-filenames3 = [ "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/SKGL1C7462UE.mp4",
-              "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/SM4TDHHC0FLL.mp4"]
-
-#filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/0WS86GPURFK5.mp4",
-#             "/Users/GretarAtli/Dropbox/ToolsForBigData/hond1/76KUS3QCGVCY.mp4"]
-
-# ur 2
-#filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/SKGL1C7462UE.mp4",
-#             "/Users/GretarAtli/Dropbox/ToolsForBigData/ur2/SM4TDHHC0FLL.mp4"] 
-
-# ur 3
-#filenames = ["/Users/GretarAtli/Dropbox/ToolsForBigData/ur3/3FVFA1DVA3NZ.mp4",
-#             "/Users/GretarAtli/Dropbox/ToolsForBigData/ur3/DD3C5S0MBKXB.mp4",
-#             "/Users/GretarAtli/Dropbox/ToolsForBigData/ur3/LS2RXLT409EG.mp4",
-#             "/Users/GretarAtli/Dropbox/ToolsForBigData/ur3/SPV675U9WWK7.mp4"]
-
-
-#video_folder_path = "/Users/GretarAtli/Dropbox/ToolsForBigData/videos"
-#video_folder_path = "/Users/GretarAtli/Dropbox/ToolsForBigData/more_than_twenty"
-#video_folder_path = "/Users/GretarAtli/Dropbox/ToolsForBigData/minor_videos"
-#video_folder_path = "/Users/helga/Dropbox/Andri/videos"
-video_folder_path = "/Users/helga/Dropbox/Andri/more_than_twenty"
 ###################### FUNCTIONS ###############################
+
+# Function that takes in a file pathm pre processes the frames of the video and then calculates 
+# a local sensitive hashing for each frame, resulting in a list of LSH values. 
+# The lsh values list/vector is then feature hashed into a vector of size N.
+# The function returns the resulting feature hash vector as well as the imageid
 def process_video (filepath):
+
+    # Read in the vide0
     reader = imageio.get_reader(filepath)
     
     # Set variables
     length = len(reader)
-    bedge_size = 250
     frames_lsh = []
     image_id = filepath.split("/")[-1].split(".")[0]
     divider = 10
+    modulus = 1
     
-    #print("--------------------------------------")
-    #print( "Video frame length: {}".format(length) )
-    #print "File name: {} ".format(image_id)
-    
-    
+    # Iterate over the frames of the video
     for counter, frame in enumerate(reader):
         
         if counter < length/divider:
-            if counter % 5 != 0:
-                                    
-                # Start analysing the frames
-                
-                frame = np.array(frame)
-                
-                #### CROP IMAGE ####
-                # Here we crop the black frame from the images
-                # There are two different cases, either a portreit image of a landscape image3
-                
-                height = np.size(frame, 0)
-                width = np.size(frame, 1)
-                
-               # print("height : {}".format(height))
-               # print("width : {}".format(width))
-                
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-            
-            
-                if height > width: # if portrait image
-                    x = 250
-                    y = 450                
-                else: # else it is a landscape 
-                    x = 450
-                    y = 250
-                
-                x_start = int(width/2 - x/2)
-                x_end = int(width/2 + x/2)
-                y_start = int(height/2 - y/2)
-                y_end = int(height/2 + y/2)
-                
-                gray = gray[y_start:y_end, x_start:x_end]
-                
-            
-            
-                #### Histogram normalization ####
-                
-                #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-                # gray = clahe.apply(gray)
-                gray = cv2.equalizeHist(gray)
-                
-                ##################################
-                
-                #gray = cv2.resize(gray,(pxls,pxls))
-                        
-                image_hash = str(imagehash.average_hash( Image.fromarray(gray), hash_size = 8))        
-                frames_lsh.append(image_hash)
+            modulus = 5                                    
+
 
         elif counter > length/divider and counter < (length/divider + length/2):
-                       
-            # Start analysing the frames
+            modulus = 1
+
+        else:
+            modulus = 5
             
+        if counter % modulus != 0:
+
+            # Start analysing the frame
             frame = np.array(frame)
             
-            #### CROP IMAGE ####
+            # -------- CROP IMAGE --------
             # Here we crop the black frame from the images
-            # There are two different cases, either a portreit image of a landscape image3
+            # There are two different cases, either a portreit image of a landscape image
             
             height = np.size(frame, 0)
             width = np.size(frame, 1)
             
-           # print("height : {}".format(height))
-           # print("width : {}".format(width))
             
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
         
@@ -191,232 +87,168 @@ def process_video (filepath):
             gray = gray[y_start:y_end, x_start:x_end]
             
         
-        
-            #### Histogram normalization ####
-            
-            #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-            # gray = clahe.apply(gray)
+            # -------- Histogram normalization --------
             gray = cv2.equalizeHist(gray)
-            
-            ##################################
-            
-            #gray = cv2.resize(gray,(pxls,pxls))
-                    
-            image_hash = str(imagehash.average_hash( Image.fromarray(gray), hash_size = 8))        
+        
+        
+            # --------- Locality Sensitive Hashing -------------
+            image_hash = str(imagehash.average_hash( Image.fromarray(gray), hash_size = 7))        
             frames_lsh.append(image_hash)
 
-        else:
-            if counter % 5 != 0:
-                 # Start analysing the frames
-                
-                frame = np.array(frame)
-                
-                #### CROP IMAGE ####
-                # Here we crop the black frame from the images
-                # There are two different cases, either a portreit image of a landscape image3
-                
-                height = np.size(frame, 0)
-                width = np.size(frame, 1)
-                
-               # print("height : {}".format(height))
-               # print("width : {}".format(width))
-                
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-            
-            
-                if height > width: # if portrait image
-                    x = 250
-                    y = 450                
-                else: # else it is a landscape 
-                    x = 450
-                    y = 250
-                
-                x_start = int(width/2 - x/2)
-                x_end = int(width/2 + x/2)
-                y_start = int(height/2 - y/2)
-                y_end = int(height/2 + y/2)
-                
-                gray = gray[y_start:y_end, x_start:x_end]
-                
-            
-            
-                #### Histogram normalization ####
-                
-                #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-                # gray = clahe.apply(gray)
-                gray = cv2.equalizeHist(gray)
-            
-            ##################################
-            
-            #gray = cv2.resize(gray,(pxls,pxls))
-                    
-            image_hash = str(imagehash.average_hash( Image.fromarray(gray), hash_size = 6))        
-            frames_lsh.append(image_hash)
-
-
         
-        
-    #print(frames_lsh)
-        
-    
     # ------------------ FEATURE HASHING -----------------------------
-    # initialize the feature hashing matrix
+    
+    # initialize the feature hashing vector
     N = 600 # number of buckets
-    
-    
     feature_hash_vector = np.zeros(N)
     
+    # Create the feature hashing vector
     for hash_str in frames_lsh:
         h = hash(hash_str)
         feature_hash_vector[h % N] += 1
         
-    
     return image_id, feature_hash_vector
 
 
-########################### STARTING ###########################
-print ("\n#################### STARTING ######################")
+print ("\n--------------------- STARTING -------------------------")
      
 if __name__ == '__main__':
-    t1 = time.time()
 
-    #for file in filenames: 
+    t1 = time.time()
+        
+    video_folder_path = "/Users/GretarAtli/Dropbox/ToolsForBigData/videos"
+
+    # Get all file paths into a list
     filepaths =  [video_folder_path + "/" + file  for file in os.listdir(video_folder_path)]
         
-    p = Pool(4)
-    results = p.map(process_video,filepaths,chunksize=1)
+    # Use the mutliprocessing to process each video
+    p = Pool(8)
+    results = p.map(process_video, filepaths, chunksize=1)
     p.close()
     p.join()
-
 
 
     t2 = time.time()
 
     print("Execution time - First part : {}".format(t2-t1))   
         
-    ######################### ANALYSE THE RESULT ###################################
-               
-    print ("\n#################### SIMILARITY ######################")
-           
-
-    #func.find_hamming_distances(results)
-    #func.find_cosine_similarity(results)
 
     ######################### CALCULATE RAND INDEX ###################################
 
            
-    print ("\n#################### TESTING RESULT ######################")       
-           
+    print ("\n-------------------- TESTING RESULT --------------------------")       
+       
     test_result = True      
-
-    n_clusters = 21
-
+    
+    n_clusters = 970
+    
     if test_result:        
            
         # make data ready for clustering algorithms
-
+    
         data = []  
         video_names = []
         for img, res in results:
             data.append(res)
             video_names.append(img)
-
+    
         print ("before calculating the clustering")
         
-        
-        print ("-------------------- Agglomeration ------------------------")
-        
-        
-        agglomerative = cluster.AgglomerativeClustering(n_clusters= n_clusters, linkage="ward").fit(data)
-        video_and_label_agglomerative = zip(agglomerative.labels_, video_names)
-        
-        clusters = defaultdict(set)
-        
-        for label, video in video_and_label_agglomerative:
-            clusters[label].add(video)
             
-        rand_index_result = func.rand_index(clusters.values())
-        
-        print(rand_index_result)
-        
-        
-        print ("--------------------- SPECTRAL ------------------------")
-        
-        
-        spectral = cluster.SpectralClustering(
-                                    n_clusters=n_clusters, eigen_solver='arpack',
-                                    affinity="nearest_neighbors", assign_labels = 'discretize', n_jobs = 4).fit(data)
-
-        
-        video_and_label_spectral = zip(spectral.labels_, video_names)
-        
-        clusters = defaultdict(set)
-        
-        for label, video in video_and_label_spectral:
-            clusters[label].add(video)
+        run_agglomerative = False
+        if run_agglomerative:
             
-        rand_index_result = func.rand_index(clusters.values())
+            print ("-------------------- Agglomeration ------------------------")
         
-        print(rand_index_result)
-        
-        
-        print ("-------------------- WARD --------------------")
-               
-        # connectivity matrix for structured Ward
-        connectivity = kneighbors_graph(
-                                    data, n_neighbors=n_clusters, include_self=False)
-        # make connectivity symmetric
-        connectivity = 0.5 * (connectivity + connectivity.T)
-
-
-        ward = cluster.AgglomerativeClustering(
-                                            n_clusters=n_clusters, linkage='ward',
-                                            connectivity=connectivity).fit(data)
-        
-        video_and_label_ward = zip(ward.labels_, video_names)
-        
-        clusters = defaultdict(set)
-        
-        for label, video in video_and_label_ward:
-            clusters[label].add(video)
+            agglomerative = cluster.AgglomerativeClustering(n_clusters= n_clusters, 
+                                                            linkage="ward").fit(data)
+            video_and_label_agglomerative = zip(agglomerative.labels_, video_names)
             
-        rand_index_result = func.rand_index(clusters.values())
-        
-        print(rand_index_result)
-        
-        
-        print ("----------------- BIRCH --------------------")
-        
-        birch = cluster.Birch(n_clusters=n_clusters).fit(data)
-        
-        video_and_label_birch = zip(birch.labels_, video_names)
-        
-        clusters = defaultdict(set)
-        
-        for label, video in video_and_label_birch:
-            clusters[label].add(video)
+            clusters = defaultdict(set)
             
-        rand_index_result = func.rand_index(clusters.values())
+            for label, video in video_and_label_agglomerative:
+                clusters[label].add(video)
+                
+            rand_index_result = rand_index_validation.rand_index(clusters.values())
+            
+            print(rand_index_result)
         
-        print(rand_index_result)
+    
+        run_spectral = True
+        if run_spectral:    
+            print ("--------------------- SPECTRAL ------------------------")
+            
+            spectral = cluster.SpectralClustering(
+                                    n_clusters=n_clusters, 
+                                    eigen_solver='arpack',
+                                    affinity="nearest_neighbors", 
+                                    assign_labels = 'discretize').fit(data)
+        
+            
+            video_and_label_spectral = zip(spectral.labels_, video_names)
+            
+            clusters = defaultdict(set)
+            
+            for label, video in video_and_label_spectral:
+                clusters[label].add(video)
+                
+            rand_index_result = rand_index_validation.rand_index(clusters.values())
+            
+            print(rand_index_result)
         
         
-        #for name, clusters in clusters_and_names:
-        #    print name
-            # print
-            # print clusters
-            # print
-        #    print type(clusters),len(clusters)
-        #    print "rand index: {}".format(rand_index(clusters))
-        #    print
-
+        
+        run_ward = False
+        if run_ward:
+            print ("-------------------- WARD --------------------")
+           
+            # connectivity matrix for structured Ward
+            connectivity = kneighbors_graph(data, n_neighbors=n_clusters, include_self=False)
+            # make connectivity symmetric
+            connectivity = 0.5 * (connectivity + connectivity.T)
+        
+        
+            ward = cluster.AgglomerativeClustering(
+                                                n_clusters=n_clusters, 
+                                                linkage='ward',
+                                                connectivity=connectivity).fit(data)
+            
+            video_and_label_ward = zip(ward.labels_, video_names)
+            
+            clusters = defaultdict(set)
+            
+            for label, video in video_and_label_ward:
+                clusters[label].add(video)
+                
+            rand_index_result = rand_index_validation.rand_index(clusters.values())
+        
+            print(rand_index_result)
+        
+        run_brich = False
+        if run_brich:
+            print ("----------------- BIRCH --------------------")
+        
+            birch = cluster.Birch(n_clusters=n_clusters).fit(data)
+            video_and_label_birch = zip(birch.labels_, video_names)
+            
+            clusters = defaultdict(set)
+            
+            for label, video in video_and_label_birch:
+                clusters[label].add(video)
+                
+            rand_index_result = rand_index_validation.rand_index(clusters.values())
+            
+            print(rand_index_result)
+        
+    
     t3 = time.time()
+    
+    print("Execution time : {}".format(t3-t1))
+        
+    
+    
+ 
 
-    print("Execution time : {}".format(t3-t2))
-        
-        
-        
-        
         
         
         
